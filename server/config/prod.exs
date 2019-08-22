1fe3ea19c388513aed4acef531418be194bc5d2d
Baris Erdem <baris@erdem.dev>
2019-08-22 22:40:21 +0300

    Configure Phoenix for distillery releases.

--- server/config/prod.exs
+++ server/config/prod.exs
@@ -11,8 +11,10 @@ use Mix.Config
 # before starting your production server.
 config :server, ServerWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-
+  url: [host: "localhost", port: System.get_env("PORT")]
+  server: true,
+  root: ".",
+  version: Application.spec(:server, :vsn)
 # Do not print debug messages in production
 config :logger, level: :info
 
