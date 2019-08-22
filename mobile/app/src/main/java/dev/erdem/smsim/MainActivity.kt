149963163cb6145c7496d5a4623a2b696cce8e1c
Baris Erdem <baris@erdem.dev>
2019-08-22 23:00:11 +0300

    QR Code scanning feature.

--- mobile/app/src/main/java/dev/erdem/smsim/MainActivity.kt
+++ mobile/app/src/main/java/dev/erdem/smsim/MainActivity.kt
@@ -5,25 +5,62 @@ import androidx.appcompat.app.AppCompatActivity
 
 import android.Manifest
 import android.app.ActivityManager
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.telephony.TelephonyManager
-import android.view.View
-import android.widget.TextView
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     internal lateinit var mServiceIntent: Intent
     private var smsPublisherService: SmsPublisherService? = null
 
+    private var cameraSource: CameraSource? = null
+
     @RequiresApi(api = Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val detector = BarcodeDetector.Builder(applicationContext).setBarcodeFormats(Barcode.DATA_MATRIX or Barcode.QR_CODE).build()
+//
+//        if (!detector.isOperational()) {
+//            Toast.makeText(applicationContext, "CAnnot read qr code.", Toast.LENGTH_LONG).show()
+//            return
+//        }
+
+        val permissions = arrayOf(Manifest.permission.CAMERA, Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS, Manifest.permission.SEND_SMS, Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_CONTACTS)
+        requestPermissions(permissions, 1)
+
+//
+//        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    Activity#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for Activity#requestPermissions for more details.
+//            Log.e("Permissions", "are not granted")
+//            return
+//        }
+
+        cameraSource = CameraSource(this, fireFaceOverlay)
+
+        cameraSource?.setMachineLearningFrameProcessor(BarcodeScanningProcessor())
+        cameraSource?.let {
+            try {
+                Log.i("cameraSource", "before start")
+                firePreview?.start(cameraSource!!, fireFaceOverlay)
+            } catch (e: IOException) {
+                Log.e("BarCode","Unable to start camera")
+                cameraSource?.release()
+                cameraSource = null
+            }
+        }
 
         smsPublisherService = SmsPublisherService(/*getApplicationContext() */)
         mServiceIntent = Intent(applicationContext, smsPublisherService!!.javaClass)
@@ -31,25 +68,6 @@ class MainActivity : AppCompatActivity() {
         if (!isSmsServiceRunning(smsPublisherService!!.javaClass)) {
             startService(mServiceIntent)
         }
-
-        val permissions = arrayOf(Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS, Manifest.permission.SEND_SMS, Manifest.permission.READ_PHONE_STATE)
-        requestPermissions(permissions, 1)
-
-
-        val phoneNumberField = findViewById<View>(R.id.phone_number) as TextView
-        val tm = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-        if (checkSelfPermission(Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    Activity#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for Activity#requestPermissions for more details.
-            return
-        }
-        val phoneNumber = tm.line1Number
-        phoneNumberField.text = phoneNumber
     }
 
     private fun isSmsServiceRunning(serviceClass: Class<*>): Boolean {
