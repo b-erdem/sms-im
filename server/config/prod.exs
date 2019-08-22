dff31238c844d2290cd6aee38083954aa5bfed72
Baris Erdem <baris@erdem.dev>
2019-08-22 22:25:00 +0300

    Remove cache manifest.

--- server/config/prod.exs
+++ server/config/prod.exs
@@ -12,7 +12,6 @@ use Mix.Config
 config :server, ServerWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
