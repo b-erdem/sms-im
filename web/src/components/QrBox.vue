eea98e8eeb96bbd12f56fd1c55a0d2c6a0a3c59f
Muhammed Erdem <iamuhammederdem@gmail.com>
2019-09-15 13:57:20 +0300

    get apk button added

--- web/src/components/QrBox.vue
+++ web/src/components/QrBox.vue
@@ -8,6 +8,7 @@
       Align the QR code within the frame to scan.
       <!-- for app: "Place the code inside the frame" -->
     </div>
+    <a href="#" target="_blank" class="qr-box__button">Get APK</a>
   </div>
 </template>
 
@@ -56,5 +57,23 @@ export default {
   font-weight: 500;
   opacity: 0.75;
   text-align: center;
+}
+
+.qr-box__button {
+  background: #fff;
+  display: flex;
+  padding: 10px 25px;
+  border-radius: 5px;
+  box-shadow: 0px 12px 30px 0px rgba(1, 50, 101, 0.3);
+  text-decoration: none;
+  color: #535357;
+  font-size: 18px;
+  font-weight: 500;
+  margin: 25px auto 10px;
+  width: 260px;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  min-height: 50px;
 }
 </style>
